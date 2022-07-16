c1a558206676d728f5240bd6b12397b4621a15ee
Dina <dinagiacobbi@gmail.com>
2022-07-16 12:24:02 -0800

    Validation Form

--- script.js
+++ script.js
@@ -19,15 +19,19 @@ function showSuccess(input) {
 }
 
 //Check email is valid (used StackOver flow ReGex)
-function isValidEmail(email) {
+function checkEmaill(input) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLocaleLowerCase());
+    if(re.test(input.value.trim()) ) {
+      showSuccess(input)
+    } else {
+      showError(input, `Email is not valid`)
+    }
 }
 
 //Check required fields
 function checkRequired(inputArray) {
-  inputArray.forEach(function (input) {
+  inputArray.forEach(function(input) {
     if (input.value.trim() === "") {
       showError(input, `${getFieldName(input)} is required`);
     } else {
@@ -35,6 +39,26 @@ function checkRequired(inputArray) {
     }
   });
 }
+
+//Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min ) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters` )
+  } else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters` )
+  } else {
+    showSuccess(input)
+  }
+}
+
+//Check passwords matching
+function checkPasswordsMatching (input1, input2) {
+  if(input1.value !== input2.value) {
+    showError(input2, `Passwords do not match.` )
+  }
+}
+
+
 //Get Field Name Function
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1)
@@ -45,4 +69,8 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
+  checkEmaill(email)
+  checkPasswordsMatching(password, password2)
 });
